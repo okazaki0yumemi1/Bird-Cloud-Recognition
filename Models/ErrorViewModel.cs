11726fbcdb65c3d3503af598e33f9409ed79985b
Dmitry Melnikov <suwako.moria@gmail.com>
2023-12-15 15:32:57 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Bird_Cloud_Recognition.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
